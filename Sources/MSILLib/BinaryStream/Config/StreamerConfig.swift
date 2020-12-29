92d5a021aae1032943d88b604847f373f261dc76
janita.a <janita.a@marketsimplified.com>
2020-12-29 21:16:00 +0530

    MSILLib changes

--- Sources/MSILLib/BinaryStream/Config/StreamerConfig.swift
+++ Sources/MSILLib/BinaryStream/Config/StreamerConfig.swift
@@ -9,12 +9,17 @@ import Foundation
 
 public struct StreamerConfig {
     
-    public init() {}
-    
     public var socketHostUrl: String!
     public var socketMode: String!
     public var socketHostPort: Int!
     public var binaryStream: Bool!
     public let socketTimeout: Int64 = Int64((Date().timeIntervalSince1970 * 2000.0).rounded())
+    
+    public init(socketHostUrl: String, socketMode: String, socketHostPort: Int, binaryStream: Bool) {
+        self.socketHostUrl = socketHostUrl
+        self.socketMode = socketMode
+        self.socketHostPort = socketHostPort
+        self.binaryStream = binaryStream
+    }
     
 }
